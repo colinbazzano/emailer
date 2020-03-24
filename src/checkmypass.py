1ac4335df332cb146a8aef3858e1d35d45b26a1c
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-03-24 10:41:58 -0400

    builds password checker that uses the pwnedpasswords api

--- src/checkmypass.py
+++ src/checkmypass.py
@@ -1,8 +1,48 @@
 import requests
 # having a browser without the browser
-
+import hashlib
+import sys
 # k animity
-url = "https://api.pwnedpasswords.com/range/" + "CBFDA"
 
-res = requests.get(url)
-print(res)
+
+def request_api_data(query_char):
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f"Error Fetching: {res.status_code}, check the api and try again.")
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            # return the count of the hash
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    # check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    # first 5, and rest
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    # print(first5_char, tail)
+    # print(response)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times... you should change your password')
+        else:
+            print(f'{password} was not found! You are good.')
+    return 'done!'
+
+
+main(sys.argv[1:])
